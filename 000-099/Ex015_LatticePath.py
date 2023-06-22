e0b79a98df721dae45b763a2b202b56bc62c7f2a
T_per <thierry.sautaux@outlook.com>
2023-06-22 12:30:15 +0200

    ex 15

---
+++ 000-099/Ex015_LatticePath.py
@@ -0,0 +1,34 @@
+"""DEFINITION
+https://projecteuler.net/problem=15
+
+Starting in the top left corner of a 2x2 grid, and only being able to move 
+to the right and down, there are exactly 6 routes to the bottom right corner.
+
+
+How many such routes are there through a 20x20 grid?
+"""
+import time;
+
+GridDimension = 20
+
+BufferList = [1]
+List = [1]
+
+ts = time.time()
+Sol = 0
+for i in range(GridDimension):
+
+    BufferList = List.copy()
+    List.append(1)
+    for j in range(1, len(BufferList),1):
+        List[j] = BufferList[j-1] + BufferList[j]
+    BufferList = List.copy()
+    List.append(1)
+    for j in range(1, len(BufferList),1):
+        List[j] = BufferList[j-1] + BufferList[j]
+    Sol = max(List)
+    
+ts = time.time() - ts
+print("Number of possible path for a Grid of {",GridDimension,";",GridDimension,"} is :",Sol)
+print("Time Elapsed :",ts,"s")
+
